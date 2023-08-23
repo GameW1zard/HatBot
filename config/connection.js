12669222d356b5142a1c47c11e43a727e902cb7e
GameW1zard <rivenloya@gmail.com>
2023-08-22 20:48:06 -0500

    uri update

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb+srv://GameW1zard:loyariven13@cluster0.i4sxasf.mongodb.net/Hatbot?retryWrites=true&w=majority');
+mongoose.connect(process.env.MONGODB_URI);
 
 export default mongoose.connection;
